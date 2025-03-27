97af6bacfd78851dfdc29de979ffec9cfeac39f7
A.K <ayyappakumarkagita184@gmail.com>
2025-03-27 17:40:42 +0000

    Assignment -1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,73 +1,28 @@
-#!/bin/sh
-# Tester script for assignment 1 and assignment 2
-# Author: Siddhant Jajoo
+#!/bin/bash
+#set +x
+numfiles=${1:-10}
+writestr=${2:-"AELD_IS_FUN"}
 
-set -e
-set -u
+# Create an empty directory
+mkdir -p /tmp/aeld-data
 
-NUMFILES=10
-WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+# Get the username from conf/username.txt
 username=$(cat conf/username.txt)
 
-if [ $# -lt 3 ]
-then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
-else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
-fi
-
-MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
-
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
-
-rm -rf "${WRITEDIR}"
-
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
-fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
-
-for i in $( seq 1 $NUMFILES)
-do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+# Loop to create numfiles files
+for i in $(seq 1 $numfiles); do
+    ./finder-app/writer.sh /tmp/aeld-data/${username}${i}.txt "$writestr"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# Run the finder script
+output=$(./finder-app/finder.sh /tmp/aeld-data "$writestr")
 
-# remove temporary directories
-rm -rf /tmp/aeld-data
+# Expected output
+expected_output="The number of files are $numfiles and the number of matching lines are $numfiles"
 
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+# Compare the output
+if [ "$output" == "$expected_output" ]; then
+    echo "success"
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "error"
 fi
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* hc_user_name = my_username();
+    const char* cnf_user_name = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hc_user_name,cnf_user_name,"username is excepted is matches with actual");
 }
